bf4ed854dbf564086e317a6b39d5d250799931a5
Kenny <khuynh2@student.fitchburgstate.edu>
2024-04-25 11:52:47 -0400

    Update 4/25

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEditor.UI;
 using UnityEngine;
 
@@ -9,19 +8,12 @@ public class Target : MonoBehaviour
     GameManager gm;
     GameObject center;
 
-    // Start is called before the first frame update
+    // Upon existing, find the Game Manager. Things won't work without it.
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
     }
-
-    public void BeenHit(RaycastHit hit, bool AccuracyEnabled)
-    {
-        hit.transform.LookAt(center.transform);
-
-        Vector2 dist = new Vector2(0, 0);
-    }
-
+    
     public void BeenHit()
     //  This overload assumes accuracy doesn't matter, and therefore we don't care how far away it was from the center.
     {
