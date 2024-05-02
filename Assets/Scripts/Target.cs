72f475f6155d36590ada41799f90b7294c06e905
Kenny <khuynh2@student.fitchburgstate.edu>
2024-05-02 03:31:19 -0400

    Adding valerie's level

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEditor.UI;
 using UnityEngine;
 
@@ -8,12 +9,19 @@ public class Target : MonoBehaviour
     GameManager gm;
     GameObject center;
 
-    // Upon existing, find the Game Manager. Things won't work without it.
+    // Start is called before the first frame update
     void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
     }
-    
+
+    public void BeenHit(RaycastHit hit, bool AccuracyEnabled)
+    {
+        hit.transform.LookAt(center.transform);
+
+        Vector2 dist = new Vector2(0, 0);
+    }
+
     public void BeenHit()
     //  This overload assumes accuracy doesn't matter, and therefore we don't care how far away it was from the center.
     {
