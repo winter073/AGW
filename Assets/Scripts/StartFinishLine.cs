90404dde7fcbcc116c4f8bf9d178ec6c29134bf6
Kenny <khuynh2@student.fitchburgstate.edu>
2024-05-08 15:21:43 -0400

    lighting finished in valerie and kenny level

--- Assets/Scripts/StartFinishLine.cs
+++ Assets/Scripts/StartFinishLine.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class StartFinishLine : MonoBehaviour
 {
-    public bool StartLine; // The designer triggers this, if it's the Start Line, this is true.
+    public bool StartLine;
     GameManager GM;
-    // Upon existing, find the Game Manager. Things won't work without it.
+    // Start is called before the first frame update
     void Start()
     {
         GM = GameObject.Find("UI").GetComponent<GameManager>();
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             GM.ChangeGameState(StartLine);
-        
+        }
     }
 }
